7e13e3558a7d4de5a70f26ee2ee0dab37cf4ffb4
Aditya Dadhich <adidadhich2003@gmail.com>
2025-01-31 17:09:32 +0530

    changed_title added url

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000','https://mlb-gamma.vercel.app'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
